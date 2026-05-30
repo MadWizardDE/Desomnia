2e157f3108166206e78b218553a6bc0e4b24c6aa
Kevin Koslowski <kevin@madwizard.de>
2026-05-30 13:19:07 +0200

    NetworkSessionMonitor: FilterRule name bug fixed

--- modules/NetworkSessionMonitor/Module.cs
+++ modules/NetworkSessionMonitor/Module.cs
@@ -2,11 +2,17 @@
 using Autofac.Core;
 using MadWizard.Desomnia.NetworkSession.Configuration;
 using MadWizard.Desomnia.NetworkSession.Manager;
+using Microsoft.Extensions.Configuration.Xml;
 
 namespace MadWizard.Desomnia.NetworkSession
 {
     public class Module : Desomnia.ConfigurableModule<ModuleConfig>
     {
+        protected override void ConfigureConfigurationSource(ExtendedXmlConfigurationSource source)
+        {
+            source.AddNamelessCollectionElement("FilterRule");
+        }
+
         protected override void Load(ContainerBuilder builder)
         {
             if (Config.NetworkSessionMonitor is not null)
