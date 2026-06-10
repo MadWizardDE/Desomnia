afad40a9f4d7e4ba0449de0bb24c1d826741037a
Kevin Koslowski <kevin@madwizard.de>
2026-06-11 00:13:53 +0200

    removed advertise configuration sidechannel

--- modules/NetworkMonitor/Configuration/Hosts/WatchedHostInfo.cs
+++ modules/NetworkMonitor/Configuration/Hosts/WatchedHostInfo.cs
@@ -37,9 +37,9 @@ namespace MadWizard.Desomnia.Network.Configuration.Hosts
 
         public virtual AdvertiseOptions MakeAdvertiseOptions(NetworkMonitorConfig network) => new()
         {
-            Type        = (Advertise            ?? network.Advertise)
-                        | (AdvertiseHostname    ?? network.AdvertiseHosts ? AdvertiseType.Host : 0)
-                        | (AdvertiseServices    ?? network.AdvertiseServices ? AdvertiseType.Service : 0),
+            Type        = (Advertise            ?? network.Advertise),
+                        //| (AdvertiseHostname    ?? network.AdvertiseHosts ? AdvertiseType.Host : 0)
+                        //| (AdvertiseServices    ?? network.AdvertiseServices ? AdvertiseType.Service : 0),
 
             Timeout     = AdvertiseTimeout      ?? network.AdvertiseTimeout,
 
